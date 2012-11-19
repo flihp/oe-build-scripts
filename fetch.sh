38ccb1ea4bbd6d2f569fcc03be493e5f67d008cc
Philip Tricca <flihp@twobit.us>
2012-11-18 20:21:44 -0500

    Find submodules dynamically in fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-for dir in meta-selinux openembedded-core bitbake; do
+grep 'submodule[[:space:]]*\".*\"]' ./.gitmodules \
+    | sed -e 's&\[submodule\s\+\"\(.*\)\"\]&\1&' \
+    | while read dir; do
+    echo "submodule: ${dir}"
     if [ ! -d $dir/.git ]; then
         git submodule init $dir
         git submodule update $dir
