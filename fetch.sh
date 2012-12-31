46fbcf016935586fedd088310dd04fe8afa01994
Philip Tricca <flihp@twobit.us>
2012-12-31 18:46:50 -0500

    Handle meta-measured in fetch.sh as a simple clone so we're always tracking the HEAD of master.  Add meta-measured to .gitignore.

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+METAS="git://github.com/flihp/meta-measured.git"
+
+# setup submodules
 grep 'submodule[[:space:]]*\".*\"]' ./.gitmodules \
     | sed -e 's&\[submodule\s\+\"\(.*\)\"\]&\1&' \
     | while read dir; do
@@ -12,4 +15,9 @@ grep 'submodule[[:space:]]*\".*\"]' ./.gitmodules \
     if [ "$1" = "update" ]; then
         (cd $dir && git checkout master && git pull origin master)
     fi
+done
+
+# clone meta layers directly from URIs
+for meta in ${METAS}; do
+    git clone ${meta}
 done
