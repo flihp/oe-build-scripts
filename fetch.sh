99344d9e99a119ac61186c3b905819b3b2583eee
Philip Tricca <flihp@twobit.us>
2013-07-25 16:31:08 -0400

    Change format of LAYERS to include a URL followed by a branch.
    fetch.sh will now clone the url and checkout the branch field.
    The loop looks a bit funny on account of accomodating LAYERS files
    without a newline before EOF.  The new format is:
    $ cat LAYERS
    url branch
    
    'branch' can be omitted and master is assumed.

--- fetch.sh
+++ fetch.sh
@@ -16,6 +16,11 @@ grep 'submodule[[:space:]]*\".*\"]' ./.gitmodules \
 done
 
 # clone meta layers directly from URIs
-for meta in $(cat ./LAYERS); do
-    git clone ${meta}
-done
+DONE=false
+until $DONE; do
+    read -r url branch || DONE=true
+    if [ ! -z ${url} ]; then
+        [ -z ${branch} ] && branch=master
+        git clone --branch ${branch} ${url}
+    fi
+done < ./LAYERS
