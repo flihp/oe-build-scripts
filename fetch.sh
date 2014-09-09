4dce2f1017dd9a337c7c7823718154d7707f4caa
Philip Tricca <flihp@twobit.us>
2014-09-09 14:49:22 +0000

    fetch.sh: Restructure LAYERS and fetch.sh accordingly.
    The goal here is to make the LAYERS file capable of being a sort of
    manifest that can be used to reproduce builds. This means allowing
    for the use of an object hash as the desired state of a repo. Next
    step is generating a LAYERS file from the current state of the build
    directory.

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# config file to store variables. Only makes sense to set GIT_MIRROR really
+# If GIT_MIRROR is set in conf file it should be a url to the basedir where
+#  this script should look for git repos.
+# example: if your LAYERS file has a url like git://github.com/foo/repo.git
+#  and you set GIT_MIRROR to file:///var/lib/git then this script will fetch
+#  from file:///var/lib/git/repo.git
+if [ -f fetch.conf ]; then
+    . ./fetch.conf
+fi
+
+GIT_MIRROR=${GIT_MIRROR:-""}
 METAS_DIR=${METAS_DIR:-./metas}
 
 usage () {
@@ -30,6 +41,11 @@ do
     esac
 done
 
+if [ ! -f ./LAYERS ]; then
+    echo "ERROR: Need a LAYERS file to clone."
+    exit 1
+fi
+
 dryrun_cmd () {
     if [ -z "${DRYRUN}" ]; then
         $@
@@ -39,80 +55,96 @@ dryrun_cmd () {
     fi
     return $?
 }
+
+# put a single repo into the specified state
+# state is determined by 'object':
+#  We clone the repo and set 'HEAD' to the specified object.
+# If object is a branch and the repo is already cloned we fetch and try to
+#  advance the branch to the latest if possible.
+# Aim to be conservative: bail if there are local changes, only advance
+#  branches if fastforward is possible.
 fetch_repo () {
     url="${1}"
-    branch="${2:-master}"
+    object="${2:-master}"
     repo_name='^.*\/\([A-Za-z_\-]\+\)\(\.git\)\?$'
-    branch_name='^[[:space:]]\?\(\*\)[[:space:]]\+\([A-Za-z0-9_\-\.]\+\)[[:space:]]*$'
     name=$(echo "${url}" | sed -n "s&${repo_name}&\1&p")
-    ret=0
+    # object from user is branch or not
+    # assume all branches specified by user are on the origin
+    git branch -a | grep -q "^\(\*\)\?[[:space:]]*remotes/origin/${object}[[:space:]]*$"
+    is_branch=$?
 
-    # fresh clone
+    # Make sure the repo is in a reasonable state
+    # if it doesn't exist just clone it
     if [ ! -d ${name} ]; then
-        echo "Cloning ${branch} from repo: ${name}"
-        dryrun_cmd git clone --progress --branch ${branch} ${url} ${name}
-        return $?
+        echo "Cloning from repo: ${name}"
+        dryrun_cmd git clone --progress ${url} ${name}
+        if [ $? -ne 0 ]; then
+            return $?
+        fi
     fi
 
-    # pull existing
-    thisdir=$(pwd)
+    local thisdir=$(pwd)
+    if [ ! -e ${name} ]; then
+        mkdir ${name}
+    fi
     cd ${name}
-    if git status | grep -q 'nothing to commit (working directory clean)'; then
-        tmp_name=$(git branch | sed -n "s&${branch_name}&\2&p")
-        if [ "${tmp_name}" = "${branch}" ]; then
-            echo "${name}: Pulling branch ${branch} --ff-only ..."
-            dryrun_cmd git pull --ff-only
-            ret=$?
-        else
-            echo "Git repo ${name} is on ${tmp_name} branch, this disagrees with LAYERS file. Not pulling."
-            ret=1
+    # an extra fetch is redundant for new clones but it doesn't hurt
+    dryrun_cmd git fetch
+    if [ ${is_branch} -eq 0 ]; then
+        # object is a branch: check it out
+        dryrun_cmd git checkout ${object}
+        if [ $? -ne 0 ]; then
+            cd ${thisdir}
+            return $?
+        fi
+        # and pull w/o merge
+        dryrun_cmd git pull --ff-only
+        if [ $? -ne 0 ]; then
+            cd ${thisdir}
+            return $?
         fi
     else
-        echo "Working dir for meta layer ${name} is dirty: not pulling."
-        ret=1
+        # not a branch: just reset hard to the object
+        dryrun_cmd git reset --hard ${object}
+        if [ $? -ne 0 ]; then
+            cd ${thisdir}
+            return $?
+        fi
     fi
     cd ${thisdir}
-
-    return ${ret}
 }
 
-# clone meta layers directly from URIs
+# Iterate over LAYERS file processing each line.
+# We treat 'bitbake' special and process it from the build root. All other
+#  repos are assumed to be meta layers and we process those in the METAS_DIR.
 fetch_repos () {
     [ ! -f ./LAYERS ] && return 0
-    . ./LAYERS
-    echo "${BITBAKE}" | while read -r url branch; do
-        if [ -z ${url} ]; then
-            echo "ERROR: BITBAKE must be set in LAYERS."
+    local thisdir=$(pwd)
+    cat ./LAYERS | \
+        grep -v '^\([[:space:]]*$\|[[:space:]]*#\)' | \
+        while read name url branch;
+    do
+        if [ -z ${url} ] | [ -z ${name} ]; then
+            echo "ERROR: format error in LAYERS file."
             exit 1
+        fi
+        # we put bitbake in the top level dir
+        # we put all other repos (meta-layers) in METAS_DIR
+        if [ ${name} != "bitbake" ]; then
+            if [ ! -d ${METAS_DIR} ]; then
+                mkdir -p ${METAS_DIR}
+            fi
+            cd ${METAS_DIR}
+        fi
+        if [ ! -z "${GIT_MIRROR}" ]; then
+            url=$(echo ${url} | sed -n "s&^.*/\([0-9A-Za-z_-]\+\(\.git\)\?\)$&${GIT_MIRROR}/\1&p")
         fi
         if ! fetch_repo "${url}" "${branch}"; then
             echo "ERROR: Failed to fetch repo from ${url}"
             exit 1
         fi
+        cd ${thisdir}
     done
-    if [ $? -ne 0 ]; then
-        exit $?
-    fi
-    if [ ! -d ${METAS_DIR} ]; then mkdir ${METAS_DIR}; fi
-    thisdir=$(pwd)
-    cd ${METAS_DIR}
-    echo "${METAS}" | while read -r url branch; do
-        if [ ! -z ${url} ]; then
-            if ! fetch_repo "${url}" "${branch}"; then
-                echo "ERROR: Failed to fetch repo from ${url}"
-                exit 1
-            fi
-        fi
-    done
-    if [ $? -ne 0 ]; then
-        exit $?
-    fi
-    cd ${thisdir}
 }
-
-if [ ! -f ./LAYERS ]; then
-    echo "ERROR: Need a LAYERS file to clone."
-    exit 1
-fi
 
 fetch_repos
