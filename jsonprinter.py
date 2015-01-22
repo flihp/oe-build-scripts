fffa29199bfcd0d66e1b6fad16e3246cdacc5176
Philip Tricca <flihp@twobit.us>
2015-01-22 15:30:55 +0000

    Create object to hold collection of repos. Clean up file handling.

--- jsonprinter.py
+++ jsonprinter.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import json
-from pprint import pprint
+
+class RepoFetcher:
+    def __init__(self, base, repos=[]):
+        self._base = base
+        self._repos = repos
+    def add_repo(self, repo):
+        self._repos.append(repo)
+    def __str__(self):
+        return ''.join(str(repo) for repo in self._repos)
+    def checkout(self):
+        raise NotImplementedError
 
 class LayerRepo:
     def __init__(self, name, url, branch="master", revision="head", layers=["./"]):
@@ -21,26 +32,22 @@ class LayerRepo:
                 "url:      {1}\n"
                 "branch:   {2}\n"
                 "revision: {3}\n"
-                "layers:   {4}".format(self._name, self._url, self._branch,
-                                       self._revision,self._layers))
+                "layers:   {4}\n".format(self._name, self._url, self._branch,
+                                         self._revision,self._layers))
 
 def main():
-    json_data = open('LAYERS.json')
-    data = json.load(json_data)
+    fetcher = RepoFetcher("./metas")
 
-    repos = []
+    with open('LAYERS.json', 'r') as json_data:
+        data = json.load(json_data)
+        for repo in data:
+            fetcher.add_repo(LayerRepo(repo["name"],
+                                       repo["url"],
+                                       repo.get("branch","master"),
+                                       repo.get("revision", "HEAD"),
+                                       repo.get("layers", ["./"])))
 
-    for repo in data:
-        repos.append(LayerRepo(repo["name"],
-                               repo["url"],
-                               repo.get("branch","master"),
-                               repo.get("revision", "HEAD"),
-                               repo.get("layers", ["./"])))
-
-    for repo in repos:
-        print repo
-
-    json_data.close()
+    print(fetcher, end='')
 
 if __name__ == '__main__':
     main()
