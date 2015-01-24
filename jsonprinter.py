549fb79b84b770270e6dc3f1928de08c920f26e6
Philip Tricca <flihp@twobit.us>
2015-01-24 19:24:39 +0000

    Comments / docstrings.

--- jsonprinter.py
+++ jsonprinter.py
@@ -16,6 +16,13 @@ class BBLayerSerializer:
     """ Class to serialize a collection of Repo objects into bblayer form.
     """
     def __init__(self, base, repos=[]):
+        """ Initialize class.
+
+        base: Directory component relative to TOPDIR where repos live.
+              For repos in ${TOPDIR}/repos the base would be 'repos'.
+        repos: An optional list of Repo objects. These objects hold all the
+               interesting data that's written to the bblayers.conf file.
+        """
         self._base = base
         self._repos = []
         for repo in repos:
@@ -24,8 +31,17 @@ class BBLayerSerializer:
             else:
                 raise TypeError
     def add_repo(self, repo):
+        """ Add Repo object to be written to the bblayers.conf file.
+
+        repo: The Repo object that's being added to the BBLayerSerializer.
+        """
         self._repos.append(repo)
     def write(self, fd=sys.stdout):
+        """ Write the bblayers.conf file to the specified file object.
+
+        fd: A file object where the bblayer.conf file will be written.
+            The default is sys.stdout.
+        """
         fd.write("LCONF_VERSION = \"5\"\n")
         fd.write("BBPATH = \"${TOPDIR}\"\n")
         fd.write("BBLAYERS = \" \\\n")
@@ -39,6 +55,11 @@ class RepoFetcher(object):
     """ Class to manage git repo state.
     """
     def __init__(self, base, repos=[]):
+        """ Initialize class.
+
+        base: Directory where repos will or currently do reside.
+        repos: List of Repo objects for the RepoFetcher to operate on.
+        """
         self._base = base
         self._repos = []
         for repo in repos:
@@ -47,10 +68,19 @@ class RepoFetcher(object):
             else:
                 raise TypeError
     def add_repo(self, repo):
+        """ Add a repo to the RepoFetcher.
+        """
         self._repos.append(repo)
     def __str__(self):
+        """ Create a string representation of all Repos in the RepoFetcher.
+        """
         return ''.join(str(repo) for repo in self._repos)
     def clone(self):
+        """ Clone all repos in a RepoFetcher.
+
+        Does nothing more than loop over the list of Repo objects invoking the
+        'clone' method on each.
+        """
         for repo in self._repos:
             repo.clone(self._base)
 
@@ -58,18 +88,37 @@ class Repo(object):
     """ Data required to clone a git repo in a specific state.
     """
     def __init__(self, name, url, branch="master", revision="head", layers=["./"]):
+        """ Initialize Repo object.
+
+        name: Sting name of the repo.
+        url: URL where git repo lives.
+        brance: Branch that will be checked out. Default is 'master'.
+        revision: Revision where HEAD should point. Default is 'HEAD'.
+        layers: A list of the OE meta-layers in the repo that we care about.
+                By default we assume the base of the repo is the root of the
+                meta layer but in some cases the repo may contain many, or none
+                at all. In this last case layers should be set to None.
+        """
         self._name = name
         self._url = url
         self._branch = branch
         self._revision = revision
         self._layers = layers
     def set_branch(self, branch):
+        """ Set branch for Repo object.
+        """
         self._branch = branch
     def set_revision(self, revision):
+        """ Set revision for Repo object.
+        """
         self._revision = revision
     def set_layers(self, layers):
+        """ Set the list of layers for the Repo ojbect.
+        """
         self._layers = layers
     def __str__(self):
+        """ Create a human readable string representation of the Repo object.
+        """
         return ("name:     {0}\n"
                 "url:      {1}\n"
                 "branch:   {2}\n"
@@ -77,6 +126,11 @@ class Repo(object):
                 "layers:   {4}\n".format(self._name, self._url, self._branch,
                                          self._revision,self._layers))
     def clone(self, path):
+        """ Clone the Repo.
+
+        path: Path where Repo will be cloned. If renative it will be relative
+              to $(pwd).
+        """
         dest = path + "/" + self._name
         try:
             if not os.path.exists(dest):
@@ -92,10 +146,15 @@ class Repo(object):
 class FetcherEncoder(JSONEncoder):
     """ Encode RepoFetcher object as JSON
 
-    Pass this class to the dumps function from the json module alnog with your
+    Pass this class to the dumps function from the json module along with your
     RepoFetcher object.
     """
     def default(self, obj):
+        """ Iterate over repo objects from RepoFetcher encoding each as JSON.
+            Return the result in a list.
+
+        obj: RepoFetcher that's being encoded as JSON.
+        """
         if type(obj) is not RepoFetcher:
             raise TypeError
         if obj._repos is None:
@@ -112,6 +171,12 @@ class RepoEncoder(JSONEncoder):
     Repo object.
     """
     def default(self, obj):
+        """ Encode a Repo object into a form suitable for serialization as
+            JSON. Basically this turns the Repo object into a native python
+            dictionary since those can be serialized to JSON.
+
+        obj: Repo object to be encoded.
+        """
         if type(obj) is not Repo:
             raise TypeError
         dict_tmp = {}
@@ -129,6 +194,7 @@ def repo_decode(json_obj):
     """ Create a repository object from a dictionary.
 
     Intended for use in JSON deserialization.
+    json_obj: A dictionary object that contains a serialized Repo object.
     """
     if type(json_obj) is not dict:
         raise TypeError
@@ -184,7 +250,7 @@ def main():
     description = "Manage OE build infrastructure."
     repos_json_help = "A JSON file describing the state of the repos."
     action_help = "An action to perform on the build directory. Possible " \
-                  "values are: setup"
+                  "values are: setup."
     source_dir_help = "Checkout git repos into this directory."
 
     parser = argparse.ArgumentParser(prog=__file__, description=description)
